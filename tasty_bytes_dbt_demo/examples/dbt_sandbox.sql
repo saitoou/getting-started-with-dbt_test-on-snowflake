ef37dd03aacf2fd7aa5a984b78197971a06f3bad
saitoou <kouhei02.3110@gmail.com>
2026-07-31 15:43:01 +0000

    fix test

--- tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
+++ tasty_bytes_dbt_demo/examples/dbt_sandbox.sql
@@ -23,3 +23,34 @@ ALTER USER GitHub_Actions_Service_User
   SET NETWORK_POLICY = github_actions_policy;
 
 SHOW PARAMETERS LIKE 'NETWORK_POLICY' FOR USER github_actions_service_user;
+
+use role accountadmin;
+
+alter user github_actions_service_user
+
+show user workload identity authentication methods
+for user github_actions_service_user;
+
+
+use role accountadmin;
+
+show grants to user github_actions_service_user;
+
+grant role accountadmin
+to user github_actions_service_user;
+
+
+use role accountadmin;
+
+show warehouses like 'TASTY_BYTES_DBT_WH';
+
+use role accountadmin;
+use warehouse TASTY_BYTES_DBT_WH;
+
+select
+    current_role(),
+    current_warehouse();
+
+show warehouses like 'TASTY_BYTES_DBT_WH';
+
+describe user GITHUB_ACTIONS_SERVICE_USER;
